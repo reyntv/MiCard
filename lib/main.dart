a66b05e98a700d8dfd90d5160b0d5472deb03987
Reynir <reynirtt@gmail.com>
2021-11-06 19:06:37 +0000

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,93 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.indigo.shade400,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 60,
+                backgroundColor: Colors.indigo.shade600,
+                child: CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/reynir.jpg'),
+                ),
+              ),
+              Text(
+                'Reynir Þorsteinsson',
+                style: TextStyle(
+                  fontFamily: 'Lobster',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Flutter wannabe-developer',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.indigo.shade900,
+                  fontSize: 12.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.indigo.shade900,
+                  thickness: 1.0,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone_android,
+                    color: Colors.indigo,
+                  ),
+                  title: Text(
+                    '+00 123 4567',
+                    style: TextStyle(
+                      color: Colors.indigo.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.indigo,
+                    ),
+                    title: Text(
+                      'rtt1@nemandi.ntv.is',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.indigo.shade900,
+                          fontFamily: 'Source Sans Pro'),
+                    ),
+                  )),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
